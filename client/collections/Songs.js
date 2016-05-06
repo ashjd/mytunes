affe018b2fa991d57df57770e4ecc9ec021073b3
ashjd <ashjd29@gmail.com>
2016-05-05 17:27:30 -0700

    update the library with data

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -3,9 +3,24 @@ var Songs = Backbone.Collection.extend({
 
   model: SongModel,
 
+  url: 'https://api.parse.com/1/classes/songs/',
+
+  parse: function(response) {
+    return response.results;
+  },
   initialize: function() {
     this.on('enqueue', function(song) {
-      console.log(song);
+      //console.log(song);
+    });
+
+    this.on('change', function(data) {
+      console.log(data);
+    });
+
+    this.fetch({
+      success: (function(model) {
+        //this.trigger('libraryChanged', model);
+      }).bind(this)
     });
   }
 });
--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -15,7 +15,7 @@ var SongQueueEntryView = Backbone.View.extend({
   template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
 
   render: function() {
-    console.log(this.model);
+    //console.log(this.model);
     return this.$el.html(this.template(this.model.attributes));
   }
   
